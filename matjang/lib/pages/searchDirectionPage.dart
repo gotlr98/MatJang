3ce488cda980593e7f332c5deeb812f80abec681
gotlr98 <gotlr98@naver.com>
2024-07-09 15:33:28 +0900

    remove direction search button & add button at search result card

--- matjang/lib/pages/searchDirectionPage.dart
+++ matjang/lib/pages/searchDirectionPage.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+import 'package:http/http.dart' as http;
 
 class SearchDirectionPage extends StatefulWidget {
   const SearchDirectionPage({super.key});
@@ -12,6 +17,38 @@ class _SearchDirectionPageState extends State<SearchDirectionPage> {
   TextEditingController fromText =
       TextEditingController(text: Get.arguments["from"]);
   TextEditingController toText = TextEditingController();
+
+  searchDirection() async {
+    var url =
+        "https://apis-navi.kakaomobility.com/v1/directions?origin=${Get.arguments["fromLat"].longitude},${Get.arguments["fromLat"].latitude}&destination=${Get.arguments["toLat"].longitude},${Get.arguments["toLat"].latitude}";
+    var header = {"Authorization": "KakaoAK ${dotenv.env["REST_API_KEY"]}"};
+    var response = await http.get(Uri.parse(url), headers: header);
+
+    if (response.statusCode == 200) {
+      var temp = jsonDecode(response.body);
+      var duration = temp["routes"][0]["summary"]["duration"];
+      print(duration);
+
+      print(convSeconds(4000));
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+  String convSeconds(int sec) {
+    if (sec < 61) {
+      return "$sec 초";
+    } else if (sec < 3600) {
+      var min = (sec / 60).floor();
+      var second = sec - min * 60;
+      return "${(min)}분 $second초";
+    } else {
+      var hour = (sec / 3600).floor();
+      var min = ((sec - hour * 3600) / 60).floor();
+      var second = sec - hour * 3600 - min * 60;
+      return "$hour시간 $min분 $second초";
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +67,10 @@ class _SearchDirectionPageState extends State<SearchDirectionPage> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           TextField(controller: fromText),
-          TextField(controller: toText)
+          TextField(controller: toText),
+          ElevatedButton(
+              onPressed: () => searchDirection(),
+              child: const Icon(Icons.search))
         ],
       ),
     );
--- matjang/lib/pages/searchResultPage.dart
+++ matjang/lib/pages/searchResultPage.dart
@@ -1,28 +1,100 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import '../model/matjip.dart';
 
-class SearchResultPage extends StatelessWidget {
+class SearchResultPage extends StatefulWidget {
   SearchResultPage({super.key, this.matjip_list});
   List<MatJip>? matjip_list;
+
+  @override
+  State<SearchResultPage> createState() => _SearchResultPageState();
+}
+
+class _SearchResultPageState extends State<SearchResultPage> {
+  Future<String> coold2Address(LatLng lng) async {
+    var url =
+        "https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lng.longitude}&y=${lng.latitude}";
+
+    var header = {"Authorization": "KakaoAK ${dotenv.env["REST_API_KEY"]}"};
+    var response = await http.get(Uri.parse(url), headers: header);
+
+    if (response.statusCode == 200) {
+      var temp = jsonDecode(response.body);
+      if (temp["documents"][0]["road_address"] != null) {
+        return temp["documents"][0]["road_address"];
+      } else if (temp["documents"][0] != null &&
+          temp["documents"][0]["address"]["address_name"] != null) {
+        return temp["documents"][0]["address"]["address_name"];
+      } else {
+        return "error";
+      }
+    } else {
+      return "error";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
         children: [
-          for (int i = 0; i < (matjip_list?.length ?? 0); i++)
+          for (int i = 0; i < (widget.matjip_list?.length ?? 0); i++) ...[
             InkWell(
               onTap: () {
-                Get.back(result: {"touch_result": matjip_list?[i]});
+                Get.back(result: {"touch_result": widget.matjip_list?[i]});
               },
               child: Card(
                 child: ListTile(
-                  title: Text(matjip_list?[i].place_name ?? ""),
-                  subtitle: Text(matjip_list?[i].address ?? ""),
+                  title: Text(widget.matjip_list?[i].place_name ?? ""),
+                  subtitle: Text(widget.matjip_list?[i].address ?? ""),
+                  trailing: ElevatedButton(
+                      onPressed: () async {
+                        bool serviceEnabled =
+                            await Geolocator.isLocationServiceEnabled();
+                        if (!serviceEnabled) {
+                          return Future.error(
+                              'Location services are disabled.');
+                        } else {
+                          LocationPermission permission =
+                              await Geolocator.checkPermission();
+                          if (permission == LocationPermission.denied) {
+                            permission = await Geolocator.requestPermission();
+                            if (permission == LocationPermission.denied) {
+                              return Future.error('permissions are denied');
+                            }
+                          } else {
+                            Position position =
+                                await Geolocator.getCurrentPosition();
+
+                            // coold2Address(
+                            //     LatLng(position.latitude, position.longitude));
+
+                            var result =
+                                await coold2Address(LatLng(37.5571, 127.0051));
+
+                            Get.toNamed("/searchDirectionPage", arguments: {
+                              "from": result,
+                              "fromLat": LatLng(
+                                  37.4826364, 126.501144), // 나중에 position으로 변경
+                              "toLat": LatLng(
+                                  double.parse(widget.matjip_list?[i].y ?? ""),
+                                  double.parse(widget.matjip_list?[i].x ?? ""))
+                            });
+                            setState(() {});
+                          }
+                        }
+                      },
+                      child: const Icon(Icons.directions)),
                 ),
               ),
             ),
+          ]
         ],
       ),
     );
